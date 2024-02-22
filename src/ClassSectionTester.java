c5bfb70cb66d388dc4b24df2881aac190de3874f
stam <144084338+c-simon-tam@users.noreply.github.com>
2024-02-22 13:29:20 +0800

    Fixed JUnit import statements

--- src/ClassSectionTester.java
+++ src/ClassSectionTester.java
@@ -1,6 +1,7 @@
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 public class ClassSectionTester {
 
